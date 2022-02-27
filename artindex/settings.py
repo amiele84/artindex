28fe41b4479d09747b9aa88616430121a231fff5
amiele84 <amiele84@gmail.com>
2022-02-27 01:39:36 -0500

    third commit

--- artindex/settings.py
+++ artindex/settings.py
@@ -14,8 +14,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) #2/26
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) #2/26
 
 
 # Quick-start development settings - unsuitable for production
