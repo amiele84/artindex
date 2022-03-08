57b6bc30455fdc4e066680cc074cb86a9068cbcd
amiele84 <amiele84@gmail.com>
2022-03-08 00:18:42 -0500

    no login-3

--- artindex/urls.py
+++ artindex/urls.py
@@ -36,7 +36,7 @@ from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('index/', include('django.contrib.auth.urls')),
+    path('index/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
